b7e218d9d015f2c41cf6d9f6ff48ad75fb0bd241
Alexander Tupavov <tauinbox@gmail.com>
2016-11-24 20:14:32 +0300

    redirection fix

--- cockroach/client/app/src/home/home.controller.js
+++ cockroach/client/app/src/home/home.controller.js
@@ -2,7 +2,26 @@
   'use strict';
 
   angular.module('cockroach')
-  .controller('HomeController', [function() {
+  .controller('HomeController', ['$location', '$state', 'menuItems', function($location, $state, menuItems) {
+
     var homeCtrl = this;
+
+    var redirect_params = $location.search();
+
+    // check for redirection parameter received from rails server
+    if (redirect_params.hasOwnProperty('goto')) {
+      // console.log(redirect_params.goto);
+
+      if ($state.get('app.' + redirect_params.goto)) {
+        // if it's some of our states, then set it to active
+        menuItems.setActive(redirect_params.goto);
+        $state.go('app.' + redirect_params.goto);
+      } else {
+        // otherwise go to home state
+        menuItems.setActive('home');
+        $state.go('app');
+      }
+    }
+
   }]);
 })();
--- cockroach/config/routes.rb
+++ cockroach/config/routes.rb
@@ -10,10 +10,8 @@ Rails.application.routes.draw do
   match "api" => proc { [404, {}, ['Invalid API endpoint']] }, :via => [:get]
   match "api/*path" => proc { [404, {}, ['Invalid API endpoint']] }, :via => [:get]  
 
-  # TODO: make it work correctly!
-  # match "/*path" => redirect("/?goto=%{path}"), :via => [:get]
-  match "/*path" => redirect("/"), :via => [:get]  
-
+  match "/*path" => redirect("/?goto=%{path}"), :via => [:get]
+  
   root to: "home#index"  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
