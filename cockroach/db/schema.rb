d33d53ec9699dd455e0d3981c499070fcf5773a0
Alexander Tupavov <tauinbox@gmail.com>
2016-12-01 14:49:09 +0300

    aply migration

--- cockroach/db/schema.rb
+++ cockroach/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101121320) do
+ActiveRecord::Schema.define(version: 20161201112710) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 20161101121320) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "nickname"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "userpic"
+    t.string   "status"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
