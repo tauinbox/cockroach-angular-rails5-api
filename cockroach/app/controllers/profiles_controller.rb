0df20d0517a233bd4a193dabf4472066d6f97500
Alexander Tupavov <tauinbox@gmail.com>
2016-12-01 15:11:12 +0300

    set actions

--- cockroach/app/controllers/profiles_controller.rb
+++ cockroach/app/controllers/profiles_controller.rb
@@ -3,18 +3,25 @@ class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :update, :destroy]
 
   def show
+    render json: @profile
   end
 
   def update
+    if @profile.update(profile_params)
+      render json: @profile
+    else
+      render json: @profile.errors, status: :unprocessable_entity
+    end    
   end
 
   def destroy
+    @profile.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_profile
-      @profile = Profile.find(params[:id])
+      @profile = Profile.find_by(user_id: params[:user_id])
     end
 
     # Only allow a trusted parameter "white list" through.
