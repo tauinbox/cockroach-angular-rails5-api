9e04014a8dba068dd19e07fa07362d80b4c6fdeb
Alexander Tupavov <tauinbox@gmail.com>
2016-12-27 14:04:56 +0300

    comment out heroku baseURL

--- cockroach/client/app/src/cockroach.module.js
+++ cockroach/client/app/src/cockroach.module.js
@@ -5,6 +5,7 @@
   angular.module('cockroach', ['ui.router', 'ui.bootstrap', 'ngResource', 'ngDialog', 'ng-token-auth'])
 
   .constant("baseURL", "http://localhost:3000/api")
+  // .constant("baseURL", "https://blablablog.herokuapp.com/api")
 
   .config(['$authProvider', 'baseURL', function($authProvider, baseURL) {
 
