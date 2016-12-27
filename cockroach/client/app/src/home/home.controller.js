762f35fef3d4ff10ae2d3c675a194ece78d7d680
Alexander Tupavov <tauinbox@gmail.com>
2016-12-27 19:07:24 +0300

    implement url redirection

--- cockroach/client/app/src/home/home.controller.js
+++ cockroach/client/app/src/home/home.controller.js
@@ -12,13 +12,42 @@
     if (redirect_params.hasOwnProperty('goto')) {
       // console.log(redirect_params.goto);
 
-      if ($state.get('app.' + redirect_params.goto)) {
-        // if it's some of our states, then set it to active
-        $state.go('app.' + redirect_params.goto);
+      // get an array of states
+      var states = $state.get();
+
+      // split received url string
+      var url_to_check = redirect_params.goto.split('/');
+
+      // we'll store there state's url for checking out
+      var target_state_url;
+
+      // we'll put there the name of appropriate state to go
+      var state_to_go;
+
+      // loop through all states to find appropriate one
+      for (var i=0; i < states.length; i++) {
+        target_state_url = states[i].url.split('/');
+        if (url_to_check.length === target_state_url.length && url_to_check[0] === target_state_url[0]) {
+          state_to_go = states[i].name;
+          break;
+        }
+      }
+
+      if (state_to_go) {
+        // if url doesn't contain any parameters
+        if (!url_to_check[1]) {
+          $state.go(state_to_go);
+        } else {
+          var param = {};
+          param[target_state_url[1].replace(':', '')] = url_to_check[1];          
+          $state.go(state_to_go, param);
+        }
+
       } else {
         // otherwise go to home state
         $state.go('app');
       }
+
     }
 
   }]);
